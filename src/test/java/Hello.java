4a98e4c8e231b98d855ea683ada896152fe99dcf
SerenKaradag <srn488@gmail.com>
2021-05-22 11:49:19 -0500

    commiting all together

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -5,6 +5,8 @@ public class Hello {
         System.out.println("Hello World");
         //enhancing hello
         //work 1
+        //work 2
+        //work 3
 
     }
 
---
+++ src/test/java/ResetPractice.java
@@ -0,0 +1,7 @@
+public class ResetPractice {
+
+    // work 5 is done
+    // work 6 is done after resetting hard way
+
+    //commiting all together
+}
